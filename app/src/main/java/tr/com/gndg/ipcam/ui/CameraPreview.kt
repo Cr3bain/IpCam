b2bfc1a86f234fd35541580fd0d49b40ff6e2a60
Ş.Ogun Gundogdu <ogun.gundogdu@gmail.com>
2025-05-03 21:43:47 +0300

    Update CameraPreview.kt
    coding

--- app/src/main/java/tr/com/gndg/ipcam/ui/CameraPreview.kt
+++ app/src/main/java/tr/com/gndg/ipcam/ui/CameraPreview.kt
@@ -1,10 +1,16 @@
 package tr.com.gndg.ipcam.ui
 
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.ImageFormat
+import android.graphics.Rect
+import android.graphics.YuvImage
 import android.util.Log
 import android.util.Size
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
@@ -16,13 +22,14 @@ import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
 import tr.com.gndg.ipcam.server.MJPEGStreamer
+import java.io.ByteArrayOutputStream
 import java.util.concurrent.Executors
 
-/*fun ImageProxy.toBitmap(): Bitmap? {
+/*fun manuelToBitmap(imageProxy: ImageProxy) : Bitmap? {
     // YUV_420_888 formatındaki görüntüyü NV21'e dönüştürme
-    val yBuffer = planes[0].buffer
-    val uBuffer = planes[1].buffer
-    val vBuffer = planes[2].buffer
+    val yBuffer = imageProxy.planes[0].buffer
+    val uBuffer = imageProxy.planes[1].buffer
+    val vBuffer = imageProxy.planes[2].buffer
 
     val ySize = yBuffer.remaining()
     val uSize = uBuffer.remaining()
@@ -34,9 +41,9 @@ import java.util.concurrent.Executors
     vBuffer.get(nv21, ySize, vSize)
     uBuffer.get(nv21, ySize + vSize, uSize)
 
-    val yuvImage = YuvImage(nv21, ImageFormat.NV21, width, height, null)
+    val yuvImage = YuvImage(nv21, ImageFormat.NV21, imageProxy.width, imageProxy.height, null)
     val out = ByteArrayOutputStream()
-    yuvImage.compressToJpeg(Rect(0, 0, width, height), 80, out)
+    yuvImage.compressToJpeg(Rect(0, 0, imageProxy.width, imageProxy.height), 80, out)
     val imageBytes = out.toByteArray()
 
     return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
@@ -75,7 +82,8 @@ fun CameraPreview(streamer: MJPEGStreamer,
                 it.setAnalyzer(executor) { imageProxy ->
                     try {
                         val bitmap = imageProxy.toBitmap()
-                        if (true) {
+                        //val bitmap = manuelToBitmap(imageProxy)
+                        if (bitmap != null) {
                             streamer.updateBitmap(bitmap)
                         }
                     } catch (e: Exception) {
